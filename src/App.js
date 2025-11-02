2874fce1f9081b0e3873d8d02b5540ccef7a846f
soyeong <mb535622@sookmyung.ac.kr>
2025-11-03 03:56:10 +0900

    feat(view): add bonus number input feature

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import { parseLottoNumbers } from "./util/parser.js";
 
 class App {
   async run() {
-    const purchaseAmount = await this.#inpuPurchaseAmount();
-    InputValidator.validatePurchaseAmount(purchaseAmount);
+    const purchaseAmount = await this.#inputPurchaseAmount();
 
     const lottoMachine = new LottoMachine(Number(purchaseAmount));
     this.#outputMessages(lottoMachine);
 
     const winningNumbers = await this.#inputWinningNumbers();
+    const bonusNumber = await this.#inputBonusNumber();
   }
 
-  async #inpuPurchaseAmount() {
+  async #inputPurchaseAmount() {
     const purchaseAmount = await inputView.readLineMessage(INPUT_MESSAGE.PURCHASE_AMOUNT);
+    InputValidator.validatePurchaseAmount(purchaseAmount);
     return purchaseAmount;
   }
 
@@ -26,6 +27,12 @@ class App {
     const winningNumbers = parseLottoNumbers(input);
     InputValidator.validateWinningNumbers(winningNumbers);
     return winningNumbers;
+  }
+
+  async #inputBonusNumber() {
+    const input = await inputView.readLineMessage(INPUT_MESSAGE.BONUS_NUMBER);
+    const bonusNumber = InputValidator.validateBonusNumber(input);
+    return bonusNumber;
   }
 
   #outputMessages(lottoMachine) {
