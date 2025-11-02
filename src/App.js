d8924559bc194518744ddebddd76c030c32d8d12
soyeong <mb535622@sookmyung.ac.kr>
2025-11-03 02:22:59 +0900

    feat(view): add winning numbers input feature

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import LottoMachine from "./model/LottoMachine.js";
 import inputView from "./view/inputView.js";
 import outputView from "./view/outputView.js";
 import { InputValidator } from "./view/validator/InputValidator.js";
+import { parseLottoNumbers } from "./util/parser.js";
 
 class App {
   async run() {
-    const purchaseAmount = await this.#inputMessages();
+    const purchaseAmount = await this.#inpuPurchaseAmount();
     InputValidator.validatePurchaseAmount(purchaseAmount);
 
     const lottoMachine = new LottoMachine(Number(purchaseAmount));
     this.#outputMessages(lottoMachine);
+
+    const winningNumbers = await this.#inputWinningNumbers();
   }
 
-  async #inputMessages() {
+  async #inpuPurchaseAmount() {
     const purchaseAmount = await inputView.readLineMessage(INPUT_MESSAGE.PURCHASE_AMOUNT);
     return purchaseAmount;
+  }
+
+  async #inputWinningNumbers() {
+    const input = await inputView.readLineMessage(INPUT_MESSAGE.WINNING_NUMBERS);
+    const winningNumbers = parseLottoNumbers(input);
+    InputValidator.validateWinningNumbers(winningNumbers);
+    return winningNumbers;
   }
 
   #outputMessages(lottoMachine) {
--- src/constant/lotto.js
+++ src/constant/lotto.js
@@ -3,4 +3,5 @@ export const LOTTO_CONFIG = Object.freeze({
     RANDOM_MAX: 45,
     RANDOM_COUNT: 6,
     LOTTO_PRICE: 1000,
+    DELIMITER: ',',
 });
